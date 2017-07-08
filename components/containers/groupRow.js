62e1dc152246bc6e441e2acb43ed4d36b8cf4cdf
Jack3113 <jack03113@gmail.com>
2017-07-08 18:48:20 +0200

    Update group list

--- components/containers/groupRow.js
+++ components/containers/groupRow.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {View, Text, TouchableHighlight, Alert} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import style from '../../Style';
 
@@ -9,11 +9,22 @@ export default class Row extends React.Component {
         index: React.PropTypes.number,
     };
 
+    displayName(name){
+        name.replace(/_/g, '');
+        return name;
+    }
+
+    openGroup(group){
+        Alert.alert(group);
+    }
+
     render() {
         return (
+        <TouchableHighlight onPress={this.openGroup(this.props.group.code)} underlayColor="white">
             <View style={[style.list.view]}>
-                    <Text>{this.props.group.name}</Text>
+                <Text>{this.displayName(this.props.group.name)}</Text>
             </View>
+        </TouchableHighlight>
         );
     }
 }
