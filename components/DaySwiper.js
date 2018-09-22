c5cc0a984372c22dcda0230f7edefa835c350457
AamuLumi <florian@kauder.fr>
2018-09-22 02:31:19 +0200

    feat(group): Add basic calendar swiper (not functional)

--- components/DaySwiper.js
+++ components/DaySwiper.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import moment from 'moment';
 import 'moment/locale/fr';
@@ -9,6 +9,10 @@ import style from '../Style';
 import Swiper from './Swiper';
 
 moment.locale('fr');
+
+function capitalize(str) {
+    return `${str.charAt(0).toUpperCase()}${str.substr(1)}`;
+}
 
 export default class DaySwiper extends React.Component {
     static navigationOptions = {
@@ -28,15 +32,29 @@ export default class DaySwiper extends React.Component {
 
         let groupName = this.props.screenProps.groupName;
 
+        const days = this.generateDays();
+
         this.state = {
             groupName,
             currentDay: currentDay,
+            currentDayIndex: days.findIndex((e) => e.isSame(currentDay)),
             index: null,
             days: [],
             renderedDays: [],
+            shownMonth: {
+                number: currentDay.month(),
+                string: capitalize(currentDay.format('MMMM')),
+            },
+            days,
+            selectedDay: currentDay,
+        };
+
+        this.viewability = {
+            itemVisiblePercentThreshold: 50,
         };
 
         this.onDayChange = this.onDayChange.bind(this);
+        this.checkViewableItems = this.checkViewableItems.bind(this);
     }
 
     componentDidMount() {
@@ -123,20 +141,116 @@ export default class DaySwiper extends React.Component {
         }
     }
 
+    generateDays() {
+        const currentDate = moment();
+        const beginningGenerationDate = moment()
+            .day(1)
+            .month(7);
+
+        if (currentDate.month() > 7) {
+            beginningGenerationDate.year(currentDate.year());
+        } else {
+            beginningGenerationDate.year(currentDate.year() - 1);
+        }
+
+        const days = [];
+
+        for (let i = 0, iMax = 365; i < iMax; i++) {
+            days.push(moment(beginningGenerationDate).add(i, 'd'));
+        }
+
+        return days;
+    }
+
+    checkViewableItems(info) {
+        if (!info.viewableItems.length) {
+            return;
+        }
+
+        for (let i = 0, iMax = info.viewableItems.length; i < iMax; i++) {
+            const date = moment(info.viewableItems[i].item);
+
+            if (date.month() < this.state.shownMonth.number) {
+                this.setState({
+                    shownMonth: {
+                        number: date.month(),
+                        string: capitalize(date.format('MMMM')),
+                    },
+                });
+
+                return;
+            }
+        }
+
+        const date = moment(info.viewableItems[0].item);
+
+        if (date.month() > this.state.shownMonth.number) {
+            this.setState({
+                shownMonth: {
+                    number: date.month(),
+                    string: capitalize(date.format('MMMM')),
+                },
+            });
+        }
+    }
+
     render() {
         if (this.state.index !== null) {
             return (
-                <Swiper
-                    ref="daySwiper"
-                    showsButtons={false}
-                    showsPagination={false}
-                    index={this.state.index}
-                    loadMinimal={true}
-                    loadMinimalSize={3}
-                    dynamic={true}
-                    onMomentumScrollEnd={this.onDayChange}>
-                    {this.state.renderedDays}
-                </Swiper>
+                <View style={{ flex: 1 }}>
+                    <DayComponent key={this.state.days[0].dayOfYear()} day={this.state.days[0]} groupName={this.state.groupName} />;
+                    <View
+                        style={{
+                            position: 'absolute',
+                            bottom: 0,
+                            backgroundColor: 'white',
+                            borderTopColor: '#DDDDDD',
+                            borderTopWidth: 1,
+                        }}>
+                        <Text style={{ textAlign: 'center', fontSize: 18, marginVertical: 8 }}>{this.state.shownMonth.string}</Text>
+                        <FlatList
+                            data={this.generateDays()}
+                            horizontal={true}
+                            keyExtractor={(item, index) => `${item.date()}-${item.month()}`}
+                            viewabilityConfig={this.viewability}
+                            onViewableItemsChanged={this.checkViewableItems}
+                            initialScrollIndex={this.state.currentDayIndex}
+                            getItemLayout={(data, index) => ({ length: 64, offset: 64 * index, index })}
+                            extraData={this.state}
+                            renderItem={({ item }) => (
+                                <View
+                                    data-month={item.month()}
+                                    style={{
+                                        width: 64,
+                                        height: 64,
+                                        paddingHorizontal: 16,
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        flex: 1,
+                                        borderTopRightRadius: 4,
+                                        borderTopLeftRadius: 4,
+                                        backgroundColor: item.isSame(this.state.selectedDay, 'day')
+                                            ? style.Theme.primary
+                                            : item.isSame(this.state.currentDay)
+                                                ? '#CCCCCC'
+                                                : 'transparent',
+                                    }}>
+                                    <Text
+                                        style={{
+                                            fontSize: 24,
+                                            marginBottom: 4,
+                                            color: item.isSame(this.state.selectedDay, 'day') ? 'white' : 'black',
+                                        }}>
+                                        {item.date()}
+                                    </Text>
+                                    <Text style={{ fontSize: 12, color: item.isSame(this.state.selectedDay, 'day') ? 'white' : 'black' }}>
+                                        {item.format('ddd')}
+                                    </Text>
+                                </View>
+                            )}
+                        />
+                    </View>
+                </View>
             );
         } else {
             return <ActivityIndicator style={style.containerView} size="large" animating={true} />;
